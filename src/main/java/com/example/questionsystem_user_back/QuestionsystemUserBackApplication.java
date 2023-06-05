303c4079d5d6647d5b4517cdaa286fa695153abc
AppleHowMuch <SecreTary_RC@hotmail.com>
2023-06-05 16:52:17 +0800

    bulid project

---
+++ src/main/java/com/example/questionsystem_user_back/QuestionsystemUserBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.questionsystem_user_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsystemUserBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionsystemUserBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/questionsystem_user_back/QuestionsystemUserBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.questionsystem_user_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsystemUserBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
